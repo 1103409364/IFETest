07159082f1c6bdb08573c351abb97c6c46d1dab8
1103409364 <1103409364@qq.com>
2018-06-02 20:55:29 +0800

    update

--- baiduIFE-31-33/JavaScript/checkbox.js
+++ baiduIFE-31-33/JavaScript/checkbox.js
@@ -1,27 +1,30 @@
 var regionWrapper = document.getElementById("region-wrapper");
 var productWrapper = document.getElementById("product-wrapper");
-
-creatCheckBox(regionWrapper, [{
-    value: 1,
+var regionCheckboxObj = [{
+    value: "华东",
     text: "华东"
 }, {
-    value: 2,
+    value: "华南",
     text: "华南"
 }, {
-    value: 3,
+    value: "华北",
     text: "华北"
-}]);
+}]
 
-creatCheckBox(productWrapper, [{
-    value: "phone",
+var productCheckboxObj = [{
+    value: "手机",
     text: "手机"
 }, {
-    value: "notebook",
+    value: "笔记本",
     text: "笔记本"
 }, {
-    value: "Google Home",
+    value: "智能音箱",
     text: "智能音箱"
-}]);
+}]
+
+creatCheckBox(regionWrapper, regionCheckboxObj);
+
+creatCheckBox(productWrapper, productCheckboxObj);
 
 function creatCheckBox(checkBoxWrapper, objData) {
     //生成全选
@@ -41,24 +44,24 @@ function creatCheckBox(checkBoxWrapper, objData) {
     var checks = []; //子选项容器
 
     for (let i = 0; i < objData.length; i++) {
-        var dataId = objData[i].text;
+        var dataId = objData[i].value;  //value值作为id
         var checkLabel = document.createElement("label");
         checkLabel.setAttribute("for", dataId);
-        checkLabel.innerHTML = dataId;
+        checkLabel.innerHTML = objData[i].text;
         checkBoxWrapper.appendChild(checkLabel);
         var childCkbox = document.createElement("input");
         childCkbox.setAttribute("type", "checkbox");
         childCkbox.setAttribute("value", objData[i].value);
         childCkbox.setAttribute("checkbox-type", "child");
-        childCkbox.setAttribute("id", objData[i].text);
+        childCkbox.setAttribute("id", dataId);
         checkBoxWrapper.appendChild(childCkbox);
         checks.push(childCkbox);
     }
 
     checks[0].checked = true; //默认选中第一项
     checkBoxWrapper.onclick = function (e) {
-        var numChecked = 0; //选中计数,默认选中一项
         var target = e.target;
+        var numChecked = 0; //选中计数
 
         if (target.type == "checkbox") {
             var attribute = target.getAttribute("checkbox-type"); //读取自定义属性
@@ -72,7 +75,6 @@ function creatCheckBox(checkBoxWrapper, objData) {
                     numChecked++;
                 }
             }
-            console.log(numChecked);
             if (attribute == "child") {
                 if (numChecked == 3) {
                     checkAll.checked = true;
@@ -81,9 +83,12 @@ function creatCheckBox(checkBoxWrapper, objData) {
                     checkAll.checked = false;
                 }
                 if (numChecked == 0) {
+                    numChecked = 1;
                     target.checked = true; //设置默认选中一项，剩余一项阻止，此时计数0
                 }
             }
+            // console.log(numChecked);
         }
+        // getData(sourceData);
     }
 }
