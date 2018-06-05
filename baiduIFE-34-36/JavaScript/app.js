6a8a43ba1c65dc3892c9de84c3eefef840229bca
1103409364 <1103409364@qq.com>
2018-06-05 21:08:26 +0800

    upload

---
+++ baiduIFE-34-36/JavaScript/app.js
@@ -0,0 +1,18 @@
+var tableHead = ["商品", "地区", "1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+var tableWrapper = document.getElementById("table-wrapper");
+var barWrapper = document.getElementById("bar-wrapper");
+
+var checkboxWrapper = document.getElementById("checkbox-wrapper");
+var eCN = document.getElementById(regionCheckboxObj[0].value);
+var sCN = document.getElementById(regionCheckboxObj[1].value);
+var nCN = document.getElementById(regionCheckboxObj[2].value);
+
+var phone = document.getElementById(productCheckboxObj[0].value);
+var notebook = document.getElementById(productCheckboxObj[1].value);
+var googleHome = document.getElementById(productCheckboxObj[2].value);
+
+var regionArr = [eCN, sCN, nCN];
+var productArr = [phone, notebook, googleHome];
+
+//默认状态初始化
+renderTable(getData(sourceData));
---
+++ baiduIFE-34-36/JavaScript/bar.js
@@ -0,0 +1,63 @@
+function drawBarGraph(data) {
+    // 画布上的坐标并未对应网页里的像素，在绘制这个1px的横线，它会把这个1px劈成两半，然后显示器根据你传来的东西会显示成模糊结果。
+    // 解决办法：如果横线，那就在y值上加个0.5。如果竖线，那就在x值上加个0.5。
+    var maxY = 210,
+        maxValue = 300;
+    // 创建svg元素需要传入命名空间，其他与创建HTML元素类似
+    var xmlns = "http://www.w3.org/2000/svg";
+    var svgDom = document.createElementNS(xmlns, "svg");
+    svgDom.setAttribute("height", "500");
+    svgDom.setAttribute("width", "800");
+    barWrapper.appendChild(svgDom);
+    // y轴最大高度210,最大值为300
+    var lineY = document.createElementNS(xmlns, "line")
+    lineY.setAttribute("x1", "10.5");
+    lineY.setAttribute("y1", "60");
+    lineY.setAttribute("x2", "10");
+    lineY.setAttribute("y2", "270");
+    lineY.setAttribute("style", "stroke:black");
+    svgDom.appendChild(lineY);
+    // y轴刻度，间距35px,分度值50,
+    for (let i = 0; i < 6; i++) {
+        let y = 60.5 + 35 * i;
+        var scaleY = document.createElementNS(xmlns, "line")
+        scaleY.setAttribute("x1", "10");
+        scaleY.setAttribute("y1", y);
+        scaleY.setAttribute("x2", "610");
+        scaleY.setAttribute("y2", y);
+        scaleY.setAttribute("style", "stroke:#dbdbdb;");
+        svgDom.appendChild(scaleY);
+    }
+
+    var lineX = document.createElementNS(xmlns, "line")
+    lineX.setAttribute("x1", "10");
+    lineX.setAttribute("y1", "270.5");
+    lineX.setAttribute("x2", "610");
+    lineX.setAttribute("y2", "270.5");
+    lineX.setAttribute("style", "stroke:black;");
+    svgDom.appendChild(lineX);
+    // x轴刻度，分度值50
+    for (let i = 1; i < 13; i++) {
+        let x = 10.5 + 50 * i;
+        var scaleY = document.createElementNS(xmlns, "line")
+        scaleY.setAttribute("x1", x);
+        scaleY.setAttribute("y1", "270");
+        scaleY.setAttribute("x2", x);
+        scaleY.setAttribute("y2", "275");
+        scaleY.setAttribute("style", "stroke:black");
+        svgDom.appendChild(scaleY);
+    }
+    // 此时data是华东地区手机12个月的数据
+    for (let i = 0; i < 12; i++) {
+        let heightData = data[i] / 300 * 210;
+        let y = 270 - heightData;
+        let x = 20 + 50 * i;
+        var rect = document.createElementNS(xmlns, "rect");
+        rect.setAttribute("x", x);
+        rect.setAttribute("y", y);
+        rect.setAttribute("width", "30");
+        rect.setAttribute("height", heightData);
+        rect.setAttribute("style", "fill:#5bc49f");
+        svgDom.appendChild(rect);
+    }
+}
