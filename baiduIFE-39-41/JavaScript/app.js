e63fe46868712195e9fa2386e94df49db9d378a9
1103409364 <1103409364@qq.com>
2018-06-13 03:08:41 +0800

    update

--- baiduIFE-39-41/JavaScript/app.js
+++ baiduIFE-39-41/JavaScript/app.js
@@ -1,5 +1,6 @@
-//默认状态初始化
+//网页载入时默认状态初始化
 renderTable(getData(sourceData));
+tableDisplayOpt(); //合并单元格，地区商品位置交换
 graph.drawGraph(getData(sourceData)); //根据复选框默认选中状态的数据画图
 graph.setData(); //监听moursehover事件画图
 
@@ -7,9 +8,10 @@ graph.setData(); //监听moursehover事件画图
 checkboxWrapper.onclick = function () {
     // 渲染表格
     renderTable(getData(sourceData));
+    tableDisplayOpt(); //合并单元格，地区商品位置交换
+    //画图
     drawMultiLineGraph(getData(sourceData));
     drawMultiBarGraph(getData(sourceData));
-    tableDisplayOpt(); //合并单元格，地区商品位置交换
 }
 tableWrapper.onmouseout = function () {
     drawMultiLineGraph(getData(sourceData));
