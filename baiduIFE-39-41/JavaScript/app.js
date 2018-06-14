96d3af8b0f2af09cc1d8a44344a2fe6b3a73b298
1103409364 <1103409364@qq.com>
2018-06-14 16:13:09 +0800

    update

--- baiduIFE-39-41/JavaScript/app.js
+++ baiduIFE-39-41/JavaScript/app.js
@@ -1,28 +1,27 @@
 //网页载入时默认状态初始化
-reDraw();
+//根据location.search的值初始化复选框
+setStatus();
+
 // 渲染表格
 renderTable(getData(sourceData));
 tableDisplayOpt(); //合并单元格，地区商品位置交换
-// 绘制统计图
 
+// 绘制统计图
 graph.drawGraph(getData(sourceData));
+
 //监听表格的moursehover事件，绘制单行数据的统计图
 graph.setData();
 
 //事件委托，监听复选框点击事件
 checkboxWrapper.onclick = function () {
-    // reDraw();
     // 复选框选项改变之后重新渲染表格
     renderTable(getData(sourceData));
     tableDisplayOpt();
     // 重新绘制统计图
     graph.drawGraph(getData(sourceData));
 }
+
 //鼠标离开表格时，重新绘制统计图
 tableWrapper.onmouseout = function () {
     graph.drawGraph(getData(sourceData));
-}
-
-
-window.onpopstate = function () {
 }
--- baiduIFE-39-41/JavaScript/state.js
+++ baiduIFE-39-41/JavaScript/state.js
@@ -1,3 +1,6 @@
+// state版本的状态保存模块，参考了Zjinxing的代码。
+// state版本是通过history.pushState()方法改变location.search的值来表示不同的状态
+// hash版本是直接操作location.hash
 function getStatus() {
     // 此函数用于获取checkbox的选中状态
     let box = document.querySelectorAll("input[type=checkbox]");
@@ -9,15 +12,15 @@ function getStatus() {
             str += "0";
         }
     }
-    console.log(str);
-
     return str;
 }
 function setStatus() {
+    // 默认选中地区和商品的第一项
     if (location.search === "") {
         location.search = "01000100";
     }
-    let str = getStatus();
+    // let str = getStatus();
+    let str = location.search;
     // 这个用来重设checkbox的选中状态
     let box = document.querySelectorAll("input[type=checkbox]");
     let strArr = location.search.slice(1).split("");
@@ -29,18 +32,31 @@ function setStatus() {
         }
     }
 }
+//监听checkbox点击事件
 checkboxWrapper.addEventListener("click", function () {
     let str = getStatus();
-    history.replaceState("state", null, "?" + str);
+    history.pushState("state", null, "?" + str);
 });
-function reDraw() {
-    let query = location.href.split("?")[1];
-    let str = getStatus();
-    if (!query) {
-        // 首先判断location.search是否存在，如果不存在则根据checkbox选中状态替换一条历史记录
-        history.replaceState("state", null, "?" + str)
-    } else {
-        // 如果存在则根据location.search设置checkbox状态，并执行以下函数
-        setStatus();
-    }
+
+//监听前进后退事件，获取状态，重设状态，重新渲染图表
+window.onpopstate = function () {
+    setStatus();
+
+    renderTable(getData(sourceData));
+    tableDisplayOpt();
+    // 绘制统计图
+    graph.drawGraph(getData(sourceData));
 }
+
+// function reDraw() {
+//     let query = location.href.split("?")[1];
+//     let str = getStatus();
+//     if (!query) {
+//         // 首先判断location.search是否存在，如果不存在则根据checkbox选中状态替换一条历史记录
+//         history.replaceState("state", null, "?" + str)
+//     } else {
+//         // 如果存在则根据location.search设置checkbox状态，并执行以下函数
+//         setStatus();
+//     }
+// }
+
