ea32ca0cc03bebcd7997d49eb439a8dc1ca026e6
1103409364 <1103409364@qq.com>
2018-07-24 14:18:52 +0800

    1

--- baiduIFE-34-36/JavaScript/HtmlUtil.js
+++ baiduIFE-34-36/JavaScript/HtmlUtil.js
@@ -1,11 +1,12 @@
 var HtmlUtil = {
-    /*1.用浏览器内部转换器实现html转码*/
+    //转码解码？有些字符需要转义比如：&lt; < \,
+    /*1.用浏览器内部转换器实现html转码：将文本转为HTML代码*/
     htmlEncode: function (html) {
         //1.首先动态创建一个容器标签元素，如DIV
         var temp = document.createElement("div");
         //2.然后将要转换的字符串设置为这个元素的innerText(ie支持)或者textContent(火狐，google支持)
         (temp.textContent != undefined) ? (temp.textContent = html) : (temp.innerText = html);
-        //3.最后返回这个元素的innerHTML，即得到经过HTML编码转换的字符串了
+        //3.最后返回这个元素的innerHTML，即得到经过  HTML编码  转换的字符串了
         var output = temp.innerHTML;
         temp = null;
         return output;
@@ -16,7 +17,7 @@ var HtmlUtil = {
         var temp = document.createElement("div");
         //2.然后将要转换的字符串设置为这个元素的innerHTML(ie，火狐，google都支持)
         temp.innerHTML = text;
-        //3.最后返回这个元素的innerText(ie支持)或者textContent(火狐，google支持)，即得到经过HTML解码的字符串了。
+        //3.最后返回这个元素的innerText(ie支持)或者textContent(火狐，google支持)，即得到经过  HTML解码  的字符串了。
         var output = temp.innerText || temp.textContent;
         temp = null;
         return output;
